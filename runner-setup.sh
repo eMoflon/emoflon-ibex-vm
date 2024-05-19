37c1fe35c389af3f361261490f54ed07cbab7b4d
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2024-05-19 15:22:28 +0200

    Runner setup script: adds curl + fixes vbox grp + fixes vagrant version

--- runner-setup.sh
+++ runner-setup.sh
@@ -6,7 +6,7 @@ USERNAME=maxkratz
 
 # utilities + sudo
 apt-get update
-apt-get install -yq sudo tmux htop wget grep sed gpg unzip tar
+apt-get install -yq sudo tmux htop wget grep sed gpg unzip tar curl
 /sbin/adduser $USERNAME sudo
 
 # VirtualBox
@@ -16,7 +16,11 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle_vbox_2016.gpg] http:/
 apt-get update
 apt-get install -yq virtualbox-7.0
 
+usermod -a -G vboxusers $USERNAME
+
 # Vagrant
-apt-get install -yq vagrant
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
+apt-get update && apt-get install -yq vagrant
 
 echo "=> Prerequisites installed. Ready for GitHub Actions runner installation."
